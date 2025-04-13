2d85610bde920560c7837d464691412f2395b925
sahibzadakashif <kashifsahibzada88@gmail.com>
2025-04-13 05:44:54 +0500

    app (1).py

--- app (1).py
+++ app (1).py
@@ -10,7 +10,7 @@ from rdkit.Chem import AllChem
 # Load model that was saved with only standard scikit-learn types
 model = joblib.load("model.pkl")
 
-st.set_page_config(page_title="NeuroPlex", layout="wide")
+st.set_page_config(page_title="GynoPotentia", layout="wide")
 
 def smiles_to_morgan_fp(smiles, radius=2, nBits=2048):
     mol = Chem.MolFromSmiles(smiles)
@@ -40,8 +40,8 @@ def get_download_link(df):
     return f'<a href="data:file/csv;base64,{b64}" download="predictions.csv">📥 Download CSV</a>'
 
 def main():
-    st.title("🧠 NeuroPlex")
-    st.markdown("AI-powered prediction of **pIC₅₀** and bioactivity class for Alzheimer's drug candidates.")
+    st.title("🚺GynoPotentia")
+    st.markdown("AI-powered prediction of **pIC₅₀** and bioactivity class for Polycystic Ovarian Syndrome (PCOS) targeting drug candidates.")
 
     input_method = st.radio("Choose Input Method", ["Paste SMILES", "Upload File"])
 
@@ -71,13 +71,56 @@ def main():
                 st.error(f"❌ Error: {e}")
 
     st.markdown("---")
-    st.markdown("#### 👨‍🔬 Team NeuroPlex")
-    st.markdown("""
-    - **Dr. Kashif Iqbal Sahibzada** – UOL & HAUT  
-    - **Dr. Andleeb Batool** – GCU Lahore  
-    - **Shumaila Shahid** – PU Lahore
-    """)
+    st.markdown("##  GynoPotentia Developers:")
+
+    # Define columns for the profiles
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
+
+    with col1:
+        st.markdown("""
+            <div style='line-height: 1.3; color: #000000;'>
+                <h3 style='color:#006a4e;'>Dr. Kashif Iqbal Sahibzada</h3>
+                Assistant Professor<br>
+                Department of Health Professional Technologies,<br>
+                Faculty of Allied Health Sciences,<br>
+                The University of Lahore<br>
+                Post-Doctoral Fellow<br>
+                Henan University of Technology, Zhengzhou, China<br>
+                <b>Email:</b> kashif.iqbal@dhpt.uol.edu.pk | kashif.iqbal@haut.edu.cn
+            </div>
+        """, unsafe_allow_html=True)
+
+     with col2:
+        st.markdown("""
+            <div style='line-height: 1.3; color: #000000;'>
+                <h3 style='color:#006a4e;'>Dr. Samiya Iqbal</h3>
+                Bechalors of Meddicine and Bachelors of Science<br>
+                School of Biochemistry and Biotechnology<br>
+                King Edward Medical University, Lahore<br>
+                <b>Email:</b> shumaila.ms.sbb@pu.edu.pk
+            </div>
+        """, unsafe_allow_html=True)
+
+    with col3:
+        st.markdown("""
+            <div style='line-height: 1.3; color: #000000;'>
+                <h3 style='color:#006a4e;'>Shumaila Shahid</h3>
+                MS Biochemistry<br>
+                School of Biochemistry and Biotechnology<br>
+                University of the Punjab, Lahore<br>
+                <b>Email:</b> shumaila.ms.sbb@pu.edu.pk
+            </div>
+        """, unsafe_allow_html=True)
+    with col4:
+        st.markdown("""
+            <div style='line-height: 1.3; color: #000000;'>
+                <h3 style='color:#006a4e;'>Muteeba Azhar</h3>
+                MS Researcher<br>
+                School of Biochemistry and Biotechnology<br>
+                University of the Punjab, Lahore<br>
+                <b>Email:</b> muteeba.azhar.sbb@pu.edu.pk
+            </div>
+        """, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
-
